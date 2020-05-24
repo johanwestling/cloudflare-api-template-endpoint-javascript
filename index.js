b6bf0c36e28f460656f00edc09d7e5ce2ca02374
Johan Westling <johan.westling@paf.com>
2020-05-24 10:25:40 +0300

    Minor cleanup

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
+
 /**
- * Respond with hello worker text
+ * Respond to endpoint calls.
  * @param {Request} request
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+async function handleRequest(request){
+  return new Response(
+    `{
+      status: 200,
+      message: 'Hello from worker endpoint'
+    }`,
+    {
+      headers: {
+        'content-type': 'text/plain'
+      }
+    }
+  );
 }
